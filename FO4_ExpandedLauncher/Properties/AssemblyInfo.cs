db2e8b6ec76137bae42ba681d7880ebe790591a1
Faith <infinity1188@yahoo.com>
2017-02-13 23:06:08 +0200

    Update Assembly info

--- FO4_ExpandedLauncher/Properties/AssemblyInfo.cs
+++ FO4_ExpandedLauncher/Properties/AssemblyInfo.cs
@@ -7,13 +7,13 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FO4_ExpandedLauncher")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ExpandedLauncher")]
+[assembly: AssemblyDescription("Improved Fallout 4 Launcher")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("https://github.com/FaithLV")]
 [assembly: AssemblyProduct("FO4_ExpandedLauncher")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("https://github.com/FaithLV")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -53,3 +53,5 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
